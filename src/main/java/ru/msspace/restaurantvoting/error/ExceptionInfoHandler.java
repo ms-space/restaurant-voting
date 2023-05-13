2a8d9f2fec81cd2006c5b486338dbf4baa61d568
Maksim Shibanov <asusib74@gmail.com>
2023-05-14 02:29:47 +0500

    add DataConflictException, ValidationException to ExceptionHandler

--- src/main/java/ru/msspace/restaurantvoting/error/ExceptionInfoHandler.java
+++ src/main/java/ru/msspace/restaurantvoting/error/ExceptionInfoHandler.java
@@ -1,6 +1,7 @@
 package ru.msspace.restaurantvoting.error;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -17,22 +18,22 @@ public class ExceptionInfoHandler {
     private static final String DATA_CONFLICT = "DB data conflict";
 
     @ExceptionHandler(NotFoundException.class)
-    public ErrorResponse notFoundError(NotFoundException ex, HttpServletRequest req) {
+    public ErrorResponse notFoundError(Exception ex, HttpServletRequest req) {
         return logAndGetErrorInfo(req, ex, HttpStatus.NOT_FOUND, NOT_FOUND);
     }
 
-    @ExceptionHandler({IllegalRequestDataException.class, BindException.class})
+    @ExceptionHandler({IllegalRequestDataException.class, BindException.class, ValidationException.class})
     public ErrorResponse illegalRequestDataError(Exception ex, HttpServletRequest req) {
         return logAndGetErrorInfo(req, ex, HttpStatus.UNPROCESSABLE_ENTITY, BAD_REQUEST);
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ErrorResponse dataIntegrityViolationError(DataIntegrityViolationException ex, HttpServletRequest req) {
+    @ExceptionHandler({DataIntegrityViolationException.class, DataConflictException.class})
+    public ErrorResponse dataConflictError(Exception ex, HttpServletRequest req) {
         return logAndGetErrorInfo(req, ex, HttpStatus.CONFLICT, DATA_CONFLICT);
     }
 
     private ErrorResponse logAndGetErrorInfo(HttpServletRequest req, Exception ex, HttpStatus status, String detail) {
-        ErrorResponse resp = ErrorResponse.builder(ex, status, detail).title("qqq").build();
+        ErrorResponse resp = ErrorResponse.builder(ex, status, detail).build();
         log.error("Exception '{}',  at request '{}'. {}",
                 detail,
                 req.getRequestURI(),
