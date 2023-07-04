65454bf8b063519fcc20b764033c4f05b30030e9
Maksim Shibanov <asusib74@gmail.com>
2023-07-05 00:43:04 +0500

    delete filed restaurantAddress from MenuTo

--- src/main/java/ru/msspace/restaurantvoting/to/MenuTo.java
+++ src/main/java/ru/msspace/restaurantvoting/to/MenuTo.java
@@ -16,19 +16,15 @@ public class MenuTo extends BaseTo {
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     int restaurantId;
 
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    String restaurantAddress;
-
     @NotNull
     LocalDate date;
 
     @NotNull
     List<Dish> dishes;
 
-    public MenuTo(Integer id, int restaurantId, String restaurantAddress, @NotNull LocalDate date, @NotNull List<Dish> dishes) {
+    public MenuTo(Integer id, int restaurantId, @NotNull LocalDate date, @NotNull List<Dish> dishes) {
         super(id);
         this.restaurantId = restaurantId;
-        this.restaurantAddress = restaurantAddress;
         this.date = date;
         this.dishes = dishes;
     }
